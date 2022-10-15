686fe8d3ed1a550babf1617c69906f76d953caba
HarshithaMaddipati <84432689+HarshithaMaddipati@users.noreply.github.com>
2022-10-15 15:27:33 +0530

    import java.util.*; class Ascii{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         double b=sc.nextDouble();         double c=a*b;         System.out.println(String.format("%.2f",c));     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Ascii{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=sc.nextDouble();
+        double c=a*b;
+        System.out.println(String.format("%.2f",c));
+    }
+} 
